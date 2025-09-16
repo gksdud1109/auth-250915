29bbdd98973afee3de90a3a0b7d3db8e116cd7df
gksdud1109 <vectorh532@gmail.com>
2025-09-16 10:12:07 +0900

    member refactor

--- src/main/java/com/rest1/domain/member/member/entity/Member.java
+++ src/main/java/com/rest1/domain/member/member/entity/Member.java
@@ -1,15 +1,9 @@
 package com.rest1.domain.member.member.entity;
 
-import com.rest1.domain.post.comment.entity.Comment;
-import com.rest1.domain.post.post.entity.Post;
 import com.rest1.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -17,13 +11,7 @@ import java.util.List;
 public class Member extends BaseEntity {
     private String username;
     private String password;
-    private String nickname;
-
-    @OneToMany(mappedBy = "author")
-    private List<Post> posts = new ArrayList<>();
-
-    @OneToMany(mappedBy = "author")
-    private List<Comment> comments = new ArrayList<>();
+    private String nickname;;
 
     public Member(String username, String password, String nickname) {
 
