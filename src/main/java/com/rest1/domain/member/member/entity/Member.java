a5af823f5fc603455258a6ca31ed15a50e6c6695
gksdud1109 <vectorh532@gmail.com>
2025-09-16 10:02:13 +0900

    member add

--- src/main/java/com/rest1/domain/member/member/entity/Member.java
+++ src/main/java/com/rest1/domain/member/member/entity/Member.java
@@ -1,9 +1,15 @@
 package com.rest1.domain.member.member.entity;
 
+import com.rest1.domain.post.comment.entity.Comment;
+import com.rest1.domain.post.post.entity.Post;
 import com.rest1.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -13,9 +19,20 @@ public class Member extends BaseEntity {
     private String password;
     private String nickname;
 
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToMany(mappedBy = "author")
+    private List<Comment> comments = new ArrayList<>();
+
     public Member(String username, String password, String nickname) {
+
         this.username = username;
         this.password = password;
         this.nickname = nickname;
+    }
+
+    public String getName(){
+        return this.nickname;
     }
 }
