6086f89771495b81282cb9ecf54792141236b0d7
gksdud1109 <vectorh532@gmail.com>
2025-09-22 12:19:02 +0900

    feat(Rq): add token validation

--- src/main/java/com/rest1/global/rq/Rq.java
+++ src/main/java/com/rest1/global/rq/Rq.java
@@ -45,7 +45,10 @@ public class Rq {
 
         Member member = null;
 
-        if (!accessToken.isBlank()) {
+        boolean isAccessTokenExists = !accessToken.isBlank();
+        boolean isAccessTokenValid = false;
+
+        if (isAccessTokenExists) {
             Map<String, Object> payload = memberService.payloadOrNull(accessToken);
 
             if (payload != null) {
@@ -54,6 +57,7 @@ public class Rq {
                 String nickname = (String) payload.get("nickname");
 
                 member = new Member(id, username, nickname);
+                isAccessTokenValid = true;
             }
         }
 
@@ -62,8 +66,16 @@ public class Rq {
                     .findByApiKey(apiKey)
                     .orElseThrow(() -> new ServiceException("401-3", "API 키가 유효하지 않습니다."));
         }
+        if (isAccessTokenExists && !isAccessTokenValid) {
+            String newAccessToken = memberService.genAccessToken(member);
+            setCookie("accessToken", newAccessToken);
+            setHeader("accessToken", newAccessToken);
+        }
 
         return member;
+    }
+    private void setHeader(String name, String value) {
+        response.setHeader(name, value);
     }
 
     private String getHeader(String name, String defaultValue) {
